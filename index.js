f2ee71644fb20564c3f30f6cf7378472e58d3681
Omer <omerfarukatas96@gmail.com>
2022-04-18 23:47:37 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+console.log(add(3, 4));
+
+const divide = function(){
+    return 2000 / 100;
+}
+
+const square = parameterSq => parameterSq ** 2;
